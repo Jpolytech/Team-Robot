973b5ccf751a960ba15149e82b054a6cd6e47f0f
Thibaut Delahaie <thibaut.delahaie@polymtl.ca>
2023-03-07 22:46:17 -0500

    mise à jour de bouton

--- lab7/lib/Bouton.cpp
+++ lab7/lib/Bouton.cpp
@@ -1,11 +1,15 @@
 #include <Bouton.h>
 
 //*******************************
-//Constructeur:
+//Constructeur par defaut:
 //Bouton sur le PORTD et PD2
 //*******************************
 Bouton::Bouton() {
     buttonIsPushed_ = false;
+    DDRD &= ~(1 << PORTD2);
+    portx_ = PIND;
+    setPos_ = PD2;
+    ddrx_ = DDRD;
 }
 
 
@@ -14,9 +18,9 @@ Bouton::Bouton() {
 //renvoie un booleen: vrai si appuyé
 /*************************/
 bool Bouton::isButtonPressed() {
-    if(PIND & (1<<PD2)){
+    if(portx_ & (1<<setPos_)){
         _delay_ms(10);
-        if(PIND & (1<<PD2)) {
+        if(portx_ & (1<<setPos_)) {
             buttonIsPushed_ = true;
             return buttonIsPushed_;
         }
@@ -27,10 +31,27 @@ bool Bouton::isButtonPressed() {
 
 
 /*************************/
-//methode qui autorise les ISR
+//methode qui autorise les ISR lors des changements d'etat du bouton
 //aucun argument et ne renvoie rien
 /*************************/
-void Bouton::allowISR() {
+void Bouton::allowISR1() {
+    cli();
+    
+    // ajustement du registre EIMSK
+    EIMSK |= (1 << INT0) ;
+
+    // Ajustement de EICRA
+    EICRA |= (1 << ISC00); //Chaque changement d'etat du bouton va generer un signal
+
+    // sei permet de recevoir à nouveau des interruptions.
+    sei();
+}
+
+/*************************/
+//methode qui autorise les ISR lorsqu'on presse sur le bouton
+//aucun argument et ne renvoie rien
+/*************************/
+void Bouton::allowISR2() {
     cli();
     
     // ajustement du registre EIMSK
@@ -51,7 +72,7 @@ void Bouton::allowISR() {
 void Bouton::newISR(vector vecteur, volatile bool& var) {
     ISR(vecteur) {
         _delay_ms(10);
-        if(PIND & (1<<PD2)) {
+        if(portx_ & (1<<setPos_)) {
             var = true;
             buttonIsPushed_ = true;
         } else{
--- lab7/lib/Bouton.h
+++ lab7/lib/Bouton.h
@@ -6,11 +6,25 @@
 class {
     public:
     Bouton();
+    Bouton(volatile uint8_t* ddrx,
+                        volatile uint8_t* pinx,
+                        uint8_t setPos) : 
+                        ddrx_(ddrx), 
+                        portx_(pinx),
+                        setPos_(setPos), 
+    {
+        *ddrx_ &= ~(1 << setPos);
+        buttonIsPushed_ = false;
+    }
     bool isButtonPressed();
-    void allowISR();
+    void allowISR1();
+    void allowISR2();
     void newISR(vector vecteur); //reverifiers
     bool getLastButtonState();
 
     private:
     bool buttonIsPushed_;
+    uint8_t portx_;
+    uint8_t setPos_;
+    uint8_t* ddrx_;
 }
