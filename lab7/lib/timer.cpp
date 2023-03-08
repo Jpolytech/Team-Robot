2cbe9a1015050665f595039c360b24b575619775
Maria Bourouisa <maria.bourouisa@polymtl.ca>
2023-03-08 13:23:05 -0500

    correction des noms de variables

--- lab7/lib/timer.cpp
+++ lab7/lib/timer.cpp
@@ -12,26 +12,25 @@ Timer::Timer() {
 //methode qui demarre la minuterie et autorise les interruptions
 //prend la duree en argument
 /*************************/
-void Timer::partirMinuterie(uint6_t duree) {
+void Timer::startTimer(uint6_t duration) {
     timerIsRunning_ = true;
     // mode CTC du timer 1 avec horloge divisée par 1024
     // interruption après la durée spécifiée
-    //mode CTC, TCNT1 et compare match
+    //mode CTC, TCNT1 et clear on compare match
     TCNT1 = 0;
-    OCR1A = duree;
-    TCCR1A = (1 << WGM12) ;
-    TCCR1B = (1 << CS12) | (1 << CS10) ;
+    OCR1A = duration;
+    TCCR1A = (1 << WGM12);
+    TCCR1B = (1 << CS12) | (1 << CS10);
     TCCR1C = 0;
-    TIMSK1 = (1 << OCIE1A) ;
+    TIMSK1 = (1 << OCIE1A);
 }
 
 /*************************/
 //methode qui arrete la minuterie
 //aucun argument et ne renvoie rien
 /*************************/
-void Timer::stopMinuterie() {
+void Timer::stopTimer() {
     TIMSK1 &= ~(1 << OCIE1A);
-    timerIsRunning_ = false;
 }
 
 /*************************/
@@ -44,15 +43,4 @@ bool Timer::IsTimerRunning() {
 
 void Timer::setTimerRunning(bool value) {
     timerIsRunning_ = value;
-}
-
-/*************************/
-//methode ISR
-//aucun argument et ne renvoie rien
-/*************************/
-void Timer::newISR(vector vecteur, volatile bool& var) {
-    ISR(TIMER1_COMPA_vect) {
-        timerIsRunning_ = false;
-        &var = false;
-    }
 }
--- lab7/lib/timer.h
+++ lab7/lib/timer.h
@@ -4,13 +4,12 @@
 
 class Timer {
     public:
-    Minuterie();
-    void partirMinuterie(uint6_t duree);
-    void stopMinuterie();
-    bool IsTimerRunning();
-    void setTimerRunning(bool value);
-    void newISR(vector vecteur, volatile bool& var); //à verifier
+        Minuterie();
+        void startTimer(uint6_t duration);
+        void stopTimer();
+        bool IsTimerRunning();
+        void setTimerRunning(bool value);
 
     private:
-        bool timerIsRunning_;
+            bool timerIsRunning_;       
 }
