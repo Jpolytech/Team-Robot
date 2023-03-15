067150f5f0ece34be2620fe536ca4559478cb699
Maria Bourouisa <maria.bourouisa@polymtl.ca>
2023-03-15 15:16:40 -0400

    version finale de l'exec

--- lab7/exec/main.cpp
+++ lab7/exec/main.cpp
@@ -1,14 +1,44 @@
-#include <avr/io.h>
+#define F_CPU 8000000UL
+#include <Led.h>
+#include <Uart.h>
+#include <util/delay.h>
+#include <Debug.h>
 
-ISR(INT0_vect)
+Led led = Led(&PORTA, &DDRA, PA0, PA1);
+ManagementUSART managementUSART;
+
+void testClasseLed()
 {
-    // Do something
+    led.switchGreen();
+    _delay_ms(2000);
+
+    led.turnedOff();
+    _delay_ms(2000);
+
+    led.switchRed();
+    _delay_ms(2000);
+
+    led.turnedOff();
+    _delay_ms(2000);
+}
+
+void testClasseUart()
+{
+    managementUSART.initialisation();
+    char word[] = "Je suis à bout :D\n";
+    managementUSART.transmissionString(word, sizeof(word));
+}
+
+void testDebug()
+{
+    DEBUG_PRINT("justine the goat\n");
 }
 
 int main()
 {
 
-    ButtonD2 button = ButtonD2();
-
-    button.enableInterrupt();
+    // decommenter chaque fonction pour tester
+    testClasseLed();
+    // testClasseUart();
+    // testDebug();
 }
